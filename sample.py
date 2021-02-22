db46e31475f0d242fdf64495d7c8ea475bd9fb96
Junpei <dpsknu25@gmail.com>
2021-02-22 22:08:07 +0900

    20210223

--- sample.py
+++ sample.py
@@ -1,12 +1,14 @@
 # print('Hello World!')
 import requests
 from bs4 import BeautifulSoup
+import time
 
 url = "https://www.bloomberg.co.jp/quote/NKY:IND"
 
-r = requests.get(url)
-
-soup = BeautifulSoup(r.content, "html.parser")
-nikkei = soup.select_one(".price")
-
-print(nikkei)
+for i in range(1000):
+    print(time.time())
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    nikkei = soup.select_one(".price")
+    print(nikkei)
+    time.sleep(1)
