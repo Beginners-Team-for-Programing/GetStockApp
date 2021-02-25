e2701e4df6ea820b1742698488439fa93aaa1e86
Junpei <dpsknu25@gmail.com>
2021-02-25 21:58:42 +0900

    20210225

--- sample.py
+++ sample.py
@@ -1,7 +1,25 @@
-# print('Hello World!')
 import requests
 from bs4 import BeautifulSoup
 import time
+import os
+from plyer import notification
+
+
+# os判別
+if os.name == 'nt':
+    print('on Windows')
+    notification.notify(
+        title = "デスクトップ通知",
+        message="windows",
+        timeout=5
+    )
+elif os.name == 'posix':
+    print('on Mac or Linux')
+    notification.notify(
+        title = "デスクトップ通知",
+        message="Mac or Linux",
+        timeout=5
+    )
 
 url = "https://www.bloomberg.co.jp/quote/NKY:IND"
 
